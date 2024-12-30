79cabd3cb6d0bc528100d7d3cd6765ffc7e83764
Andrew Thornton <andrewthornton9619@gmail.com>
2024-12-30 18:16:30 +1030

    adding reset signal

--- logic/and_gate_cocotb/and_gate_cocotb.py
+++ logic/and_gate_cocotb/and_gate_cocotb.py
@@ -3,18 +3,26 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer
 
 async def generate_clock(dut):
     """Generate clock pulses."""
-    dut.srst_i.value = 0
-    for cycle in range(20):
+    while(True):
         dut.clk_i.value = 0
         await Timer(1, units="ns")
         dut.clk_i.value = 1
         await Timer(1, units="ns")
 
+async def generate_reset(dut):
+    """Generate clock pulses."""
+    dut.srst_i.value = 1
+    for i in range(10):
+        await RisingEdge(dut.clk_i)
+    dut.srst_i.value = 0
 
 @cocotb.test()
 async def and_gate_all_values_test(dut):
     """Try accessing the design."""
     cocotb.fork(generate_clock(dut))
+    cocotb.fork(generate_reset(dut))
+
+    await FallingEdge(dut.srst_i)
     await FallingEdge(dut.clk_i)
 
     #test 1
