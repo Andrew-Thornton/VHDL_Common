d68c688abbd1d154b16cf6d3da07da1448480465
Andrew Thornton <andrewthornton9619@gmail.com>
2025-10-11 13:12:15 +1030

    working with gtk wave

---
+++ floating_point/float_mult_simple_tb.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#Compile the source files
+ghdl -a --std=08 common_float_tools_pkg.vhd
+ghdl -a --std=08 float_mult.vhd
+
+#Compile the test bench
+ghdl -a --std=08 float_mult_simple_tb.vhd
+
+#elaborate the testbench
+ghdl -e --std=08 float_mult_simple_tb
+
+#run the testbench\
+ghdl -r --std=08 float_mult_simple_tb --vcd=float_mult_simple_tb_wave.vcd --stop-time=1us
+
+gtkwave float_mult_simple_tb_wave.vcd float_mult_simple_tb.gtkw &
